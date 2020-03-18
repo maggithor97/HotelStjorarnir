9c09a897fcc322f47bf12ae8bd85d05da154f100
maggithor97 <maggithor1@gmail.com>
2020-03-18 17:08:39 +0000

    Grunnur af klösum

---
+++ classes/BookingController.java
@@ -0,0 +1,29 @@
+public class BookingController {
+    private Booking booking;
+
+    public BookingController() {
+        booking = New Booking();
+    }
+
+    public addBooking(....){
+
+    }
+
+    public cancelBooking(Booking booking) {
+
+    }
+
+    public modifyBooking(Booking booking) {
+
+    }
+    public getBooking (int referenceNr) {
+
+    }
+    public getAllBookings(Hotel hotel) {
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
